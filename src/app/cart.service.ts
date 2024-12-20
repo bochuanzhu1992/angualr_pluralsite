731c4800b45ac3ff7856894bc4ecba73f8377680
zhubochuan <2536167503@qq.com>
2024-12-20 15:58:14 +1100

    ''

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
 import { ILineItem} from './catalog/line-item.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cart: ILineItem[]= [];
-  constructor() { }
+  private cart: IProduct[]= [];
+  constructor(private http: HttpClient) { }
 
   add(product: IProduct){
-    let lineItem = this.findLineItem(product);
-    if (lineItem !== undefined){
-      lineItem.qty++;
-    }else{
-      lineItem = {product: product, qty:1};
-      this.cart.push(lineItem);
-    }
+   this.cart.push(product);
+   this.http.post('/api/cart', this.cart).subscribe(()=>{
+    console.log('done');
+   });
   }
 
-  findLineItem(product:IProduct){
-    return this.cart.find((li)=> li.product.id === product.id)
-  }
+ 
 }
