471f2d78d42eee0ba43fa30589648006b34a2895
zhubochuan <2536167503@qq.com>
2024-12-20 16:27:13 +1100

    ''

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,20 +2,36 @@ import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
 import { ILineItem} from './catalog/line-item.model';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cart: IProduct[]= [];
-  constructor(private http: HttpClient) { }
+  private cart: BehaviorSubject<IProduct[]> = new BehaviorSubject<IProduct[]>([]);
+  constructor(private http: HttpClient) {
+    this.http.get<IProduct[]>('/api/cart').subscribe({
+      next: (cart)=>this.cart.next(cart),
+    });
+   }
 
+  getCart(): Observable<IProduct[]>{
+    return this.cart.asObservable();
+  }
   add(product: IProduct){
-   this.cart.push(product);
+   const newCart = [...this.cart.getValue(), product];
+   this.cart.next(newCart);
    this.http.post('/api/cart', this.cart).subscribe(()=>{
     console.log('done');
    });
   }
 
+  remove(product:IProduct){
+    let newCart = this.cart.getValue().filter((i)=> i!==product);
+    this.cart.next(newCart);
+    this.http.post('/api/cart', newCart).subscribe(()=>{
+      console.log('removed'+product.name+' from cart!');
+    });
+  }
  
 }
